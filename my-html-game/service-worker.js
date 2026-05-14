6168770c87f1ea38ac8ba2da91b231f37c472b24
Marek Micko <marek.micko11@gmail.com>
2026-05-14 17:21:45 +0200

    Update service-worker.js with better offline support

--- my-html-game/service-worker.js
+++ my-html-game/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'html-game-v1';
+const CACHE_NAME = 'html-game-v2';
 const urlsToCache = [
     './',
     './index.html',
@@ -10,26 +10,56 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching app shell');
+            return cache.addAll(urlsToCache);
+        })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => 
-            response || fetch(event.request)
-        ).catch(() => caches.match('./index.html'))
-    );
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.filter((name) => name !== CACHE_NAME)
-                    .map((name) => caches.delete(name))
-            );
+        caches.match(event.request).then((response) => {
+            if (response) {
+                console.log('Serving from cache:', event.request.url);
+                return response;
+            }
+            
+            return fetch(event.request).then((response) => {
+                if (!response || response.status !== 200 || response.type === 'error') {
+                    return response;
+                }
+                
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                
+                return response;
+            });
+        }).catch(() => {
+            console.log('Network request failed, returning cached index.html');
+            return caches.match('./index.html');
         })
     );
 });

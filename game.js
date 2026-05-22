e10dca1aa976a3ee2da1d1b96b94175ad0468e94
kkaomie <marek.micko11@gmail.com>
2026-05-22 16:23:32 +0200

    cipos 3.0

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-function initGame(level) {
+window.initGame = function initGame(level) {
+      console.log('game.js loaded: initGame registered');
       const canvas = document.getElementById('gameCanvas');
       const ctx = canvas.getContext('2d');
       
